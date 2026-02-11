0130b078fa61ca7d805d11027ede22c29b7c2496
PurvaPotbhare <purvapotbhare4@gmail.com>
2026-02-11 15:05:23 +0530

    initial commit

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,74 +1,3 @@
-# # from dotenv import load_dotenv
-# # import os
-
-# # load_dotenv()
-
-# # from fastapi import FastAPI
-# # from fastapi.middleware.cors import CORSMiddleware
-# # from app.routes.prospect import router
-
-# # app = FastAPI(title="Persona Prospect Finder")
-
-# # # CORS FIX
-# # app.add_middleware(
-# #     CORSMiddleware,
-# #     allow_origins=[
-# #         "http://localhost:5173",
-# #         "http://127.0.0.1:5173"
-# #     ],
-# #     allow_credentials=True,
-# #     allow_methods=["*"],
-# #     allow_headers=["*"],
-# # )
-# # from app.services.sheets_exporter import export_to_sheets
-
-# # @app.get("/force-sheet-test")
-# # def force_sheet_test():
-# #     export_to_sheets([
-# #         ["FINAL TEST", "SUCCESS", "CEO", "TestCo", "India", 100]
-# #     ])
-# #     return {"status": "done"}
-
-
-# # app.include_router(router)
-# from dotenv import load_dotenv
-# import os
-
-# load_dotenv()
-
-# from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
-# from app.routes.prospect import router
-# from app.services.sheets_exporter import export_to_sheets
-
-# app = FastAPI(title="Persona Prospect Finder")
-
-# CLIENT_URL = os.getenv("CLIENT_URL", "http://localhost:5173")
-
-# # ✅ CORS configuration
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=[
-#         CLIENT_URL,
-#         "http://localhost:5173",
-#         "http://127.0.0.1:5173"
-#     ],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-# @app.get("/")
-# def health():
-#     return {"status": "API is running"}
-
-# @app.get("/force-sheet-test")
-# def force_sheet_test():
-#     export_to_sheets([
-#         ["FINAL TEST", "SUCCESS", "CEO", "TestCo", "India", 100]
-#     ])
-#     return {"status": "done"}
-
-# app.include_router(router)
 from dotenv import load_dotenv
 import os
 
@@ -80,11 +9,12 @@ from app.routes.prospect import router
 
 app = FastAPI(title="Persona Prospect Finder")
 
-CLIENT_URL = os.getenv("CLIENT_URL", "http://localhost:5173")
-print(CLIENT_URL);
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[CLIENT_URL],
+    allow_origins=[
+        "https://persona-prospect-finders.vercel.app",  
+        "http://localhost:5173"                          
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
